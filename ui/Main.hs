ea5a55ad2aecbd110696a8e3c6a035b6a04fd834
Jean-Baptiste Mazon <jb@yavin>
2022-01-10 00:13:34 +0100

    Migrate styling from Pango markup and GtkLinkButtons to GTK tags

--- ui/Main.hs
+++ ui/Main.hs
@@ -1,35 +1,43 @@
-{-# LANGUAGE ImplicitParams #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Main where
+
+import Debug.Trace
 
 import Data.GI.Base (AttrOp((:=)))
 import qualified Data.GI.Base as GI
 import qualified GI.GLib as GLib
 import qualified GI.Gdk as Gdk
 import qualified GI.Gtk as Gtk
+import qualified GI.Pango as Pango
 
+import Control.Applicative
 import Control.Concurrent
 import Control.Lens
 import Control.Monad
 import Control.Monad.Logger
 import Control.Monad.Reader
-import Data.Foldable
+import Control.Monad.Extra hiding (unit)
+import Control.Monad.Trans.Maybe
+import Data.Int
 import Data.IORef
+import Data.Maybe
 import Data.Text (Text)
 import Data.Time
 import Data.Time.Format.ISO8601
 import qualified Data.Text as Text
 import Database.Persist.Sqlite
+import Foreign.Ptr
+import Foreign.StablePtr
 import Text.URI
 import Text.Megaparsec (parseMaybe)
+import Unsafe.Coerce
 
 import Format
 import Lib
 
 import Cache
 import History
-import LoremIpsum
 
 data Browser = Browser
   { _history :: !(IORef History)
@@ -43,7 +51,7 @@ data BrowserGtk = BrowserGtk
   , _forwardButton :: !Gtk.ToolButton
   , _refreshButton :: !Gtk.ToolButton
   , _urlEntry :: !Gtk.Entry
-  , _textContent :: !Gtk.TextView
+  , _textView :: !Gtk.TextView
   , _statusBar :: !Gtk.Statusbar
   }
 
@@ -106,12 +114,26 @@ main = runStderrLoggingT $ withSqliteConn "gemini.db" $ \db -> do
     -- home page filler
     Just textViewGO <- #getObject builder "body-view"
     Just textViewGtk <- GI.castTo Gtk.TextView textViewGO
-    buf <- #getBuffer textViewGtk
-    GI.set buf [ #text := loremIpsum ]
+    hover <- fmap castStablePtrToPtr . newStablePtr =<< newIORef Nothing
+    #setDataFull textViewGtk "hovering" hover (Just (freeStablePtr . castPtrToStablePtr))
+    void (GI.on textViewGtk #motionNotifyEvent (motionNotifyEvent textViewGtk))
 
-    -- Status: just to connect signals
+    -- Statusbar: just to connect signals
     Just statusbarGO <- #getObject builder "statusbar"
     Just statusbarGtk <- GI.castTo Gtk.Statusbar statusbarGO
+
+    -- <pre>: set nowrap (may be buggy in Glade)
+    Just preGO <- #getObject builder "pre"
+    Just preGtk <- GI.castTo Gtk.TextTag preGO
+    GI.set preGtk [ #wrapMode := Gtk.WrapModeNone ]
+
+    -- <li>: set tab array (until I find out whether it's possible in GtkBuilder)
+    Just liGO <- #getObject builder "li"
+    Just liGtk <- GI.castTo Gtk.TextTag liGO
+    liIndent <- GI.get liGtk #indent
+    tabs <- Pango.tabArrayNew 1 True
+    #setTab tabs 0 Pango.TabAlignLeft (abs liIndent)
+    GI.set liGtk [ #tabs := tabs ]
 
     pure BrowserGtk
       { _window = windowGtk
@@ -120,7 +142,7 @@ main = runStderrLoggingT $ withSqliteConn "gemini.db" $ \db -> do
       , _refreshButton = refreshButtonGtk
       , _urlEntry = urlEntryGtk
       , _statusBar = statusbarGtk
-      , _textContent = textViewGtk
+      , _textView = textViewGtk
       }
 
   his <- liftIO (newIORef emptyHistory)
@@ -136,7 +158,7 @@ main = runStderrLoggingT $ withSqliteConn "gemini.db" $ \db -> do
       urlBuf <- GI.get (browser ^. gtk . urlEntry) #buffer
       urlText <- GI.get urlBuf #text
       case parseMaybe @Text parser urlText >>= validateGeminiURI of
-        Just gUri -> followLink browser gUri
+        Just gUri -> followGeminiLink browser gUri
         Nothing -> do
           -- Gtk.messageDialogNew missing from bindings
           dialog <- GI.new Gtk.MessageDialog [ #buttons := Gtk.ButtonsTypeOk
@@ -151,29 +173,33 @@ main = runStderrLoggingT $ withSqliteConn "gemini.db" $ \db -> do
 
     void $ GI.on (browser ^. gtk . backButton) #clicked $ do
       h <- readIORef (browser ^. history)
-      forM_ (moveBack h) (openLink False browser)
+      forM_ (moveBack h) (openGeminiLink False browser)
 
     void $ GI.on (browser ^. gtk . forwardButton) #clicked $ do
       h <- readIORef (browser ^. history)
-      forM_ (moveForward h) (openLink False browser)
+      forM_ (moveForward h) (openGeminiLink False browser)
 
     void $ GI.on (browser ^. gtk . refreshButton) #clicked $
-      openLink True browser =<< readIORef (browser ^. history)
+      openGeminiLink True browser =<< readIORef (browser ^. history)
+
+    void $ GI.on (browser ^. gtk . textView) #eventAfter (eventAfter browser)
+    void $ GI.on (browser ^. gtk . textView) #queryTooltip (queryTooltip browser)
+    void $ GI.on (browser ^. gtk . textView) #populatePopup (populatePopup browser)
 
     #showAll (browser ^. gtk . window)
     Gtk.main
 
-followLink :: Browser -> GeminiURI -> IO ()
-followLink browser url = do
+followGeminiLink :: Browser -> GeminiURI -> IO ()
+followGeminiLink browser url = do
   his' <- push url <$> readIORef (browser ^. history)
-  openLink False browser his'
+  openGeminiLink False browser his'
 
-openLink :: Bool -> Browser -> History -> IO ()
-openLink refresh browser his = forM_ (his ^. current) $ \url ->
+openGeminiLink :: Bool -> Browser -> History -> IO ()
+openGeminiLink refresh browser his = forM_ (his ^. current) $ \url ->
   void $ forkIO $ do
     runReaderT (fetchLink refresh url) browser >>= \case
       Left errMsg -> toGtk $ do
-        buf <- #getBuffer (browser ^. gtk . textContent)
+        buf <- #getBuffer (browser ^. gtk . textView)
         GI.set buf [ #text := errMsg ]
         let sb = browser ^. gtk . statusBar
         ctx <- #getContextId sb "page"
@@ -194,7 +220,7 @@ openLink refresh browser his = forM_ (his ^. current) $ \url ->
         toGtk $ do
           urlBuf <- GI.get (browser ^. gtk . urlEntry) #buffer
           GI.set urlBuf [ #text := render (url ^. _Wrapped) ]
-          gmiToGtk url doc (browser ^. gtk . textContent) (followLink browser)
+          gmiToGtk url doc (browser ^. gtk . textView)
           let sb = browser ^. gtk . statusBar
           ctx <- #getContextId sb "page"
           void (#push sb ctx status)
@@ -223,39 +249,44 @@ toGtk :: IO a -> IO ()
 toGtk a = void $ Gdk.threadsAddIdle GLib.PRIORITY_DEFAULT (False <$ a)
 
 -- It's not gmiToPango because links are GtkLinkButtons.
-gmiToGtk :: GeminiURI -> Doc -> Gtk.TextView -> (GeminiURI -> IO ()) -> IO ()
-gmiToGtk base Doc {docLines} tv follow = do
+gmiToGtk :: GeminiURI -> Doc -> Gtk.TextView -> IO ()
+gmiToGtk base Doc {docLines} tv = do
   buf <- #getBuffer tv
   uncurry (#delete buf) =<< #getBounds buf
   iter <- #getStartIter buf
+  let insertLn txt = do
+        #insert buf iter txt (-1)
+        #insert buf iter "\n" 1
+
+  fromMark <- #createMark buf Nothing iter True
+  let tagged tag block = do
+        #moveMark buf fromMark iter
+        block
+        fromIter <- #getIterAtMark buf fromMark
+        #applyTagByName buf tag fromIter iter
+
   forM_ docLines $ \case
-    TextLine (SimpleTextLine l) -> #insert buf iter (l <> "\n") (-1)
-    TextLine (HeadingLine lvl h) -> do
-      let big = 2 - fromEnum lvl
-      esc <- GLib.markupEscapeText h (-1)
-      #insertMarkup buf iter
-        (fold (replicate big "<big>") <> "<b>"
-         <> esc
-         <> "</b>" <> fold (replicate big "</big>")
-         <> "\n")
-        (-1)
-    TextLine (ListItem li) -> #insert buf iter ("\t• " <> li <> "\n") (-1)
-    TextLine (QuoteLine q) -> do
-      esc <- GLib.markupEscapeText q (-1)
-      #insertMarkup buf iter ("<i>" <> esc <> "</i>\n") (-1)
+    TextLine (SimpleTextLine l) -> insertLn l
+    TextLine (HeadingLine lvl h) ->
+      let tagName = case lvl of
+                      L1 -> "h1"
+                      L2 -> "h2"
+                      L3 -> "h3"
+      in tagged tagName $ insertLn h
+    TextLine (ListItem li) -> tagged "li" $ #insert buf iter "•\t" (-1) *> insertLn li
+    TextLine (QuoteLine q) -> tagged "q" $ insertLn q
     LinkLine uri mbDesc -> do
-      anchor <- #createChildAnchor buf iter
-      #insert buf iter "\n" 1
       let uriT = render uri
-      link <- Gtk.linkButtonNewWithLabel uriT mbDesc
-      case validateGeminiURI =<< uri `relativeTo` unGemini base of
-        Just gUri -> void $ GI.on link #activateLink $ True <$ follow gUri
-        Nothing -> pure ()
-      #show link
-      #addChildAtAnchor tv link anchor
-    PreBlock _ preBlock -> do
-      esc <- GLib.markupEscapeText (Text.unlines preBlock) (-1)
-      #insertMarkup buf iter ("<tt>" <> esc <> "</tt>") (-1)
+      tagged "href" $ insertLn (fromMaybe uriT mbDesc)
+      startIter <- #getIterAtMark buf fromMark
+      uriTag <- GI.new Gtk.TextTag []
+      tbl <- GI.get buf #tagTable
+      void (#add tbl uriTag)
+      let Just absUri = uri `relativeTo` unGemini base
+      href <- castStablePtrToPtr <$> newStablePtr (absUri :: HRefValue)
+      #setDataFull uriTag "href" href (Just (freeStablePtr . castPtrToStablePtr))
+      #applyTag buf uriTag startIter iter
+    PreBlock _ preBlock -> tagged "pre" $ #insert buf iter (Text.unlines preBlock) (-1)
 
 formatDelta :: NominalDiffTime -> String
 formatDelta delta
@@ -270,3 +301,118 @@ formatDelta delta
              | otherwise =         (secs `div` (365*86400),"year")
     secs = round (nominalDiffTimeToSeconds delta)
     s = if n == 1 then "" else "s"
+
+setCursorIfAppropriate :: Gtk.TextView -> Int32 -> Int32 -> IO ()
+setCursorIfAppropriate tv x y = do
+  (gotIter,iter) <- #getIterAtLocation tv x y
+  hovering <- whenMaybe gotIter $ do
+               tags <- Gtk.textIterGetTags iter
+               mbUri <- flip firstJustM tags $ \tag -> runMaybeT $ do
+                         uriT <- #getData tag "href"
+                         guard (uriT /= nullPtr)
+                         liftIO (deRefStablePtr (castPtrToStablePtr uriT))
+               pure (maybe (Left ()) Right mbUri)
+  wasHoveringRef <- deRefStablePtr . castPtrToStablePtr =<< #getData tv "hovering"
+  wasHovering <- readIORef wasHoveringRef :: IO (Maybe (Either () URI))
+  when (hovering /= wasHovering) $ do
+    writeIORef wasHoveringRef hovering
+    display <- #getDisplay tv
+    handCursor <- Gdk.cursorNewFromName display "pointer"
+    regularCursor <- Gdk.cursorNewFromName display "text"
+    Just gdkWin <- #getWindow tv Gtk.TextWindowTypeText
+    #setCursor gdkWin $ case hovering of
+      Nothing -> Nothing
+      Just (Right _) -> handCursor
+      Just (Left ())  -> regularCursor
+
+motionNotifyEvent :: Gtk.TextView -> Gtk.WidgetMotionNotifyEventCallback
+motionNotifyEvent self eventMotion = do
+  ex <- round <$> GI.get eventMotion #x
+  ey <- round <$> GI.get eventMotion #y
+  (x,y) <- #windowToBufferCoords self Gtk.TextWindowTypeWidget ex ey
+  setCursorIfAppropriate self x y
+  pure False
+
+eventAfter :: Browser -> Gtk.WidgetEventAfterCallback
+eventAfter browser event = fmap (fromMaybe ()) $ runMaybeT $ do
+  et <- GI.get event #type
+  (ex,ey) <- case et of
+              Gdk.EventTypeButtonRelease -> do
+                let event' = unsafeCoerce event :: Gdk.EventButton
+                button <- GI.get event' #button
+                if fromIntegral button /= Gdk.BUTTON_PRIMARY
+                  then empty
+                  else do
+                    ex <- round <$> GI.get event' #x
+                    ey <- round <$> GI.get event' #y
+                    pure (ex,ey)
+              Gdk.EventTypeTouchEnd -> do
+                let event' = unsafeCoerce event :: Gdk.EventTouch
+                ex <- round <$> GI.get event' #x
+                ey <- round <$> GI.get event' #y
+                pure (ex,ey)
+              _ -> empty
+  let tv = browser ^. gtk . textView
+  buffer <- GI.get tv #buffer
+  hasSelection <- GI.get buffer #hasSelection
+  guard (not hasSelection)
+
+  (x,y) <- #windowToBufferCoords tv Gtk.TextWindowTypeWidget ex ey
+  (gotIter,iter) <- #getIterAtLocation tv x y
+  when gotIter $ followIfLink browser iter
+
+type HRefValue = URI
+
+followIfLink :: MonadIO m => Browser -> Gtk.TextIter -> m ()
+followIfLink browser iter = do
+  tags <- #getTags iter
+  mbUri <- flip firstJustM tags $ \tag -> runMaybeT $ do
+    uriT <- #getData tag "href"
+    guard (uriT /= nullPtr)
+    liftIO (deRefStablePtr (castPtrToStablePtr uriT))
+  mapM_ (openLink browser) mbUri
+
+openLink :: MonadIO m => Browser -> URI -> m ()
+openLink browser uri = case validateGeminiURI uri of
+  Just guri -> liftIO (followGeminiLink browser guri)
+  Nothing -> do
+    let win = browser ^. gtk . window
+    Gtk.showUriOnWindow (Just win) (render uri) (fromIntegral Gdk.CURRENT_TIME)
+
+queryTooltip :: Browser -> Gtk.WidgetQueryTooltipCallback
+queryTooltip _ _ _ True _ = traceM "keyboard mode tooltip" *> pure False
+queryTooltip browser ex ey False tooltip = fmap (maybe False (const True)) $ runMaybeT $ do
+  let tv = browser ^. gtk . textView
+  (x,y) <- #windowToBufferCoords tv Gtk.TextWindowTypeWidget ex ey
+  (gotIter,iter) <- #getIterAtLocation tv x y
+  guard gotIter
+  tags <- #getTags iter
+  uri <- MaybeT $ flip firstJustM tags $ \tag -> runMaybeT $ do
+    uriT <- #getData tag "href"
+    guard (uriT /= nullPtr)
+    liftIO (deRefStablePtr (castPtrToStablePtr uriT))
+  Gtk.tooltipSetText tooltip (Just (render uri))
+
+populatePopup :: Browser -> Gtk.TextViewPopulatePopupCallback
+populatePopup browser widget = do
+  hoveringPtr <- #getData (browser ^. gtk . textView) "hovering"
+  guard (hoveringPtr /= nullPtr)
+  mbHovering <- readIORef =<< deRefStablePtr (castPtrToStablePtr hoveringPtr)
+  case mbHovering of
+    Just (Right url) -> do
+      Just menu <- GI.castTo Gtk.Menu widget
+      menuItemCopy <- Gtk.menuItemNewWithMnemonic "_Copy URL"
+      void $ GI.on menuItemCopy #activate $ copyActivate browser (render url)
+      #show menuItemCopy
+      #append menu menuItemCopy
+      menuItemOpen <- Gtk.menuItemNewWithMnemonic "_Open URL"
+      void $ GI.on menuItemOpen #activate $ openLink browser url
+      #show menuItemOpen
+      #append menu menuItemOpen
+    _ -> pure ()
+
+copyActivate :: Browser -> Text -> Gtk.MenuItemActivateCallback
+copyActivate browser uri = do
+  clipboardAtom <- Gdk.atomIntern "CLIPBOARD" False
+  clipboard <- #getClipboard (browser ^. gtk . textView) clipboardAtom
+  #setText clipboard uri (-1)
